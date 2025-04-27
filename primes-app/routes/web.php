12088cf5ee45e8da15ad01e3513392c72fc80d42
FranklinJFrame <franklinrp23@gmail.com>
2025-04-26 21:42:00 -0400

    Update web.php

--- primes-app/routes/web.php
+++ primes-app/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+use App\Http\Controllers\UserController;
+
+Route::get('/frankas', [UserController::class, 'index']);
