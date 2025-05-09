fc132f96ec7c0f24f944f331ad5cac360aeab8e4
LuisEnrique03 <132832333+LuisEnrique03@users.noreply.github.com>
2025-05-09 09:17:44 -0400

    Filtro
    Se añadio un filtro general de pedidos organizados por todos, nuevo, procesado, entregado y cancelado

--- primes-app/app/Filament/Resources/PedidosResource/Pages/ListPedidos.php
+++ primes-app/app/Filament/Resources/PedidosResource/Pages/ListPedidos.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\PedidosResource\Pages;
 use App\Filament\Resources\PedidosResource;
 use App\Filament\Resources\OrderResource\Widgets\PedidosStats;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListPedidos extends ListRecords
@@ -21,6 +22,16 @@ class ListPedidos extends ListRecords
     protected function getHeaderWidgets(): array {
         return [
             PedidosStats::class,
+        ];
+    }
+
+    public function getTabs(): array {
+        return [
+            null => Tab::make('Todos'),
+            'Nuevo' => Tab::Make()->query(fn ($query) => $query->where('estado', 'nuevo')),
+            'Procesando' => Tab::Make()->query(fn ($query) => $query->where('estado', 'procesando')),
+            'Enviado' => Tab::Make()->query(fn ($query) => $query->where('estado', 'enviado')),
+            'Cancelado' => Tab::Make()->query(fn ($query) => $query->where('estado', 'cancelado')),
         ];
     }
 }
