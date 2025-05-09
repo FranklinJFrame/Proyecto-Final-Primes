c8ba848d77d1a5a9a8fec13c5bc182881a5b1fc4
LuisEnrique03 <132832333+LuisEnrique03@users.noreply.github.com>
2025-05-09 09:00:15 -0400

    Nuevos campos agregados a pedidos
    Se agregaron los campos de nuevos pedidos, procesamiento de pedidos, peidos enviados y precio promedio

--- primes-app/app/Filament/Resources/OrderResource/Widgets/PedidosStats.php
+++ primes-app/app/Filament/Resources/OrderResource/Widgets/PedidosStats.php
@@ -5,13 +5,17 @@ namespace App\Filament\Resources\OrderResource\Widgets;
 use App\Models\Pedidos;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Number;
 
 class PedidosStats extends BaseWidget
 {
     protected function getStats(): array
     {
         return [
-            Stat::make('Nuevos pedidos', Pedidos::where('estado', 'new')->count()),
+            Stat::make('Nuevos Pedidos', Pedidos::where('estado', 'nuevo')->count()),
+            Stat::make('Procesamiento de Pedidos', Pedidos::where('estado', 'procesando')->count()),
+            Stat::make('Pedido Enviado', Pedidos::where('estado', 'enviado')->count()),
+            Stat::make('Precio Promedio', Number::currency(Pedidos::query()->avg('total_general')), 'INR')
         ];
     }
 }
