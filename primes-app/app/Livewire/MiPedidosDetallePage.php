b8cbdd3aa3e33ca9973a984fc1a3f778be83f6b9
4ngeto <145989843+4ngeto@users.noreply.github.com>
2025-05-14 00:26:39 -0400

    Añadí Carrito, Checkout, DetalleProducto, MisPedidos, MiPedidosDetalle, Productos,

---
+++ primes-app/app/Livewire/MiPedidosDetallePage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class MiPedidosDetallePage extends Component
+{
+    public function render()
+    {
+        return view('livewire.mi-pedidos-detalle-page');
+    }
+}
--- primes-app/routes/web.php
+++ primes-app/routes/web.php
@@ -2,6 +2,12 @@
 
 use App\Livewire\HomePage;
 use App\Livewire\CategoriesPage;
+use App\Livewire\ProductosPage;
+use App\Livewire\CarritoPage;
+use App\Livewire\DetalleProductoPage;
+use App\Livewire\CheckoutPage;
+use App\Livewire\MisPedidosPage;
+use App\Livewire\MiPedidosDetallePage;
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -19,3 +25,9 @@ require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
 
 Route::get('/categories', CategoriesPage::class);
+Route::get('/products', ProductosPage::class);
+Route::get('/cart', CarritoPage::class);
+Route::get('/products/{product}', DetalleProductoPage::class);
+Route::get('/checkout', CheckoutPage::class);
+Route::get('/my-orders', MisPedidosPage::class);
+Route::get('/my-orders/{order}', MiPedidosDetallePage::class);
