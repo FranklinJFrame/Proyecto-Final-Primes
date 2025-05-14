08f5ad1118d9fcb7a4a46a4acc09ae86314d80e5
4ngeto <145989843+4ngeto@users.noreply.github.com>
2025-05-13 23:30:31 -0400

    Añadí categorías
    Añadí categorías, aún está estático, luego añadiré el dinamismo

--- primes-app/resources/views/components/layouts/app.blade.php
+++ primes-app/resources/views/components/layouts/app.blade.php
@@ -7,11 +7,15 @@
         <title>{{ $title ?? 'Page Title' }}</title>
         @vite(['resources/css/app.css', 'resources/js/app.js']) <!-- Correct file reference -->
         @livewireStyles
+        <script src="https://cdn.tailwindcss.com"></script>
+
     </head>
     <body class="bg-slate-200 dark:bg-slate-700">
+        @livewire('partials.navbar')
         <main>
             {{ $slot }}
         </main>
+        @livewire('partials.footer')
         @livewireScripts
     </body>
 </html>
--- primes-app/routes/web.php
+++ primes-app/routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Livewire\HomePage;
+use App\Livewire\CategoriesPage;
+
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 //primes-app\app\Livewire\HomePage.php
 Route::get('/', HomePage::class);
+
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
@@ -14,3 +17,5 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
+
+Route::get('/categories', CategoriesPage::class);
