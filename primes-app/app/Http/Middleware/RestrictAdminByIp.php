82e623c502e69bbdc4169bf7e65b6847b02dff1f
LuisEnrique03 <132832333+LuisEnrique03@users.noreply.github.com>
2025-05-29 23:38:49 -0400

    Update RestrictAdminByIp.php

--- primes-app/app/Http/Middleware/RestrictAdminByIp.php
+++ primes-app/app/Http/Middleware/RestrictAdminByIp.php
@@ -10,8 +10,7 @@ class RestrictAdminByIp
 {
     // List of allowed IPs
     protected $allowedIps = [
-        //'127.0.0.1',
-        //'::1',
+        '10.0.0.9',
         // Add more as needed
     ];
 
