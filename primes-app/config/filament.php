60b81ddd159262e8eef5ddea27a9290e55606d64
FranklinJFrame <franklinrp23@gmail.com>
2025-05-29 23:04:50 -0400

    Update filament.php

--- primes-app/config/filament.php
+++ primes-app/config/filament.php
@@ -3,10 +3,6 @@
 return [
     'asset_url' => env('ASSET_URL', env('APP_URL')),
     'domain' => env('FILAMENT_DOMAIN'),
-    'home_url' => env('FILAMENT_HOME_URL', '/'),'
-return [
-    'asset_url' => env('ASSET_URL', env('APP_URL')),
-    'domain' => env('FILAMENT_DOMAIN'),
     'home_url' => env('FILAMENT_HOME_URL', '/'),
     'path' => env('FILAMENT_PATH', 'admin'),
     'middleware' => [
