56c7bf086cabfb9078544b6147f7712cce242b92
FranklinJFrame <franklinrp23@gmail.com>
2025-05-29 22:01:07 -0400

    Update app-layout.blade.php

--- primes-app/resources/views/components/app-layout.blade.php
+++ primes-app/resources/views/components/app-layout.blade.php
@@ -5,10 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js']) <!-- Correct file reference -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
-        <script src="https://cdn.tailwindcss.com"></script>
-
     </head>
     <body class="bg-slate-200 dark:bg-slate-700">
         @livewire('partials.navbar')
@@ -17,5 +15,8 @@
         </main>
         @livewire('partials.footer')
         @livewireScripts
+        <script src="https://cdn.tailwindcss.com"></script>
+        <!-- Fallback para JS de Vite si @vite falla -->
+        <script src="{{ asset('build/assets/app-1VjoYaU3.js') }}" type="module"></script>
     </body>
 </html> 
