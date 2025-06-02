13876106304c0fc801a5f54aa5c9223e8fff6623
FranklinJFrame <franklinrp23@gmail.com>
2025-06-02 08:31:05 -0400

    Update LowStockProductos.php

--- primes-app/app/Filament/Widgets/LowStockProductos.php
+++ primes-app/app/Filament/Widgets/LowStockProductos.php
@@ -14,6 +14,9 @@ class LowStockProductos extends BaseWidget
 {
     protected int | string | array $columnSpan = 'full';
 
+    protected static bool $shouldRegister = true;
+    protected static ?int $pollingInterval = 60; // Actualizar cada 60 segundos
+
     public function mount(): void
     {
         $count = Producto::where('cantidad', '<=', 5)->count();
@@ -25,6 +28,7 @@ class LowStockProductos extends BaseWidget
                 ->danger()
                 ->icon('heroicon-o-exclamation-triangle')
                 ->persistent()
+                ->autoDismiss(false)
                 ->actions([
                     Action::make('ver')
                         ->label('Ver productos con bajo stock')
