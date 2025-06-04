229bd2419de74728605586c60802297659444bed
FranklinJFrame <franklinrp23@gmail.com>
2025-06-04 09:24:48 -0400

    Update filesystems.php

--- primes-app/config/filesystems.php
+++ primes-app/config/filesystems.php
@@ -66,6 +66,13 @@ return [
             'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
         ],
 
+        'cloud' => [
+            'driver' => 'cloudinary',
+            'api_key'    => env('CLOUDINARY_API_KEY'),
+            'api_secret' => env('CLOUDINARY_API_SECRET'),
+            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        ],
+
     ],
 
     /*
