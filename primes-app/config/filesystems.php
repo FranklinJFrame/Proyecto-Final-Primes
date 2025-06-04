34db0779b23ec1f1822f5f3f25129dca2429aaf7
FranklinJFrame <franklinrp23@gmail.com>
2025-06-04 09:32:40 -0400

    Revert "Update filesystems.php"
    This reverts commit 229bd2419de74728605586c60802297659444bed.

--- primes-app/config/filesystems.php
+++ primes-app/config/filesystems.php
@@ -66,13 +66,6 @@ return [
             'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
         ],
 
-        'cloud' => [
-            'driver' => 'cloudinary',
-            'api_key'    => env('CLOUDINARY_API_KEY'),
-            'api_secret' => env('CLOUDINARY_API_SECRET'),
-            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-        ],
-
     ],
 
     /*
