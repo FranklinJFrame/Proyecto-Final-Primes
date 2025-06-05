7c5d1505f03040829d1ab59fa1af4fd12465378d
FranklinJFrame <franklinrp23@gmail.com>
2025-06-05 19:40:53 -0400

    Update AppServiceProvider.php

--- primes-app/app/Providers/AppServiceProvider.php
+++ primes-app/app/Providers/AppServiceProvider.php
@@ -25,22 +25,8 @@ class AppServiceProvider extends ServiceProvider
         URL::forceScheme('https');
 
         // Configurar Livewire para usar HTTPS
-        $appUrl = config('app.url');
-        Config::set('livewire.asset_url', $appUrl);
-        Config::set('livewire.app_url', $appUrl);
-        Config::set('livewire.middleware_group', 'web');
-        Config::set('livewire.temporary_file_upload', [
-            'disk' => 'local',
-            'rules' => ['required', 'file', 'max:12288'],
-            'directory' => 'livewire-tmp',
-            'middleware' => 'throttle:60,1',
-            'preview_mimes' => [
-                'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
-                'mov', 'avi', 'wmv', 'mp3', 'm4a',
-                'jpg', 'jpeg', 'mpga', 'webp', 'wma',
-            ],
-            'max_upload_time' => 5,
-        ]);
+        Config::set('livewire.asset_url', config('app.url'));
+        Config::set('livewire.app_url', config('app.url'));
 
         if ($this->app->environment('production')) {
             $url = 'https://proyecto-final-primes-production-96c3.up.railway.app';
