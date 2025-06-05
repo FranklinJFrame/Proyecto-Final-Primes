1bd7a989ee498826bd0962ecc9fe06e28d7437f5
FranklinJFrame <franklinrp23@gmail.com>
2025-06-05 14:37:38 -0400

    Revert "Update filament.php"
    This reverts commit d1e6b9cb831867bcd7e766b9a90967037506a00b.

--- primes-app/config/filament.php
+++ primes-app/config/filament.php
@@ -12,9 +12,4 @@ return [
             'scripts' => true,
         ],
     ],
-    'auth' => [
-        'guard' => 'web',
-    ],
-    'domain' => env('APP_URL', 'https://proyecto-final-primes-production-96c3.up.railway.app'),
-    'url' => env('APP_URL', 'https://proyecto-final-primes-production-96c3.up.railway.app'),
 ];
