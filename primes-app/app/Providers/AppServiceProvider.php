cd9dbfe02939b135ee6ecfd8db9cd267ddb7040c
FranklinJFrame <franklinrp23@gmail.com>
2025-06-05 19:43:02 -0400

    Revert "Update AppServiceProvider.php"
    This reverts commit 5c2a2abe77ce6e2a40c47d2492d30eef01497a36.

--- primes-app/app/Providers/AppServiceProvider.php
+++ primes-app/app/Providers/AppServiceProvider.php
@@ -21,29 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Forzar HTTPS en todos los entornos
-        URL::forceScheme('https');
-
-        // Configurar Livewire para usar HTTPS
-        $appUrl = config('app.url');
-        Config::set('livewire.asset_url', $appUrl);
-        Config::set('livewire.app_url', $appUrl);
-        Config::set('livewire.middleware_group', 'web');
-        Config::set('livewire.temporary_file_upload', [
-            'disk' => 'local',
-            'rules' => ['required', 'file', 'max:12288'],
-            'directory' => 'livewire-tmp',
-            'middleware' => 'throttle:60,1',
-            'preview_mimes' => [
-                'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
-                'mov', 'avi', 'wmv', 'mp3', 'm4a',
-                'jpg', 'jpeg', 'mpga', 'webp', 'wma',
-            ],
-            'max_upload_time' => 5,
-        ]);
-
         if ($this->app->environment('production')) {
             $url = 'https://proyecto-final-primes-production-96c3.up.railway.app';
+
+            // Forzar HTTPS
+            URL::forceScheme('https');
 
             // Establecer URL base
             Config::set('app.url', $url);
