45ec58bf4800e7b7859061b725b2e62e160095a6
FranklinJFrame <franklinrp23@gmail.com>
2025-06-05 14:34:40 -0400

    Reapply "fixed https"
    This reverts commit 84e41c8b0a726d9822873cb7f8c40bd979e57df5.

--- primes-app/resources/views/components/layouts/app.blade.php
+++ primes-app/resources/views/components/layouts/app.blade.php
@@ -5,10 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
         <title>{{ $title ?? 'Page Title' }}</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js']) <!-- Correct file reference -->
+        
+        @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/admin/theme.css'])
         @livewireStyles
+        @filamentStyles
+        
         <script src="https://cdn.tailwindcss.com"></script>
-
     </head>
     <body class="bg-slate-200 dark:bg-slate-700">
         @livewire('partials.navbar')
@@ -17,6 +19,7 @@
         </main>
         @livewire('partials.footer')
         @livewireScripts
+        @filamentScripts
     </body>
 </html>
 
