2aa10dd7d58a7003c0958ee11b4b7eabf6bdcac8
FranklinJFrame <franklinrp23@gmail.com>
2025-06-05 22:54:38 -0400

    Update livewire.php

---
+++ primes-app/config/livewire.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'class_namespace' => 'App\\Livewire',
+    'view_path' => resource_path('views/livewire'),
+    'layout' => 'layouts.app',
+    'temporary_file_upload' => [
+        'disk' => 'local',
+        'rules' => ['required', 'file', 'max:12288'],
+        'directory' => 'livewire-tmp',
+        'middleware' => ['web', 'auth'],
+        'preview_mimes' => [
+            'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
+            'mov', 'avi', 'wmv', 'mp3', 'm4a',
+            'jpg', 'jpeg', 'mpga', 'webp', 'wma',
+        ],
+        'max_upload_time' => 5,
+    ],
+    'manifest_path' => null,
+    'back_button_cache' => false,
+    'render_on_redirect' => false,
+];
