de64f5d241ae343e517be85881c2943b1f7e21fa
FranklinJFrame <franklinrp23@gmail.com>
2025-06-06 19:22:56 -0400

    Update filesystems.php

--- primes-app/config/filesystems.php
+++ primes-app/config/filesystems.php
@@ -61,9 +61,11 @@ return [
 
         'cloudinary' => [
             'driver' => 'cloudinary',
-            'api_key' => env('CLOUDINARY_API_KEY'),
-            'api_secret' => env('CLOUDINARY_API_SECRET'),
-            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+            'cloud' => [
+                'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+                'api_key'    => env('CLOUDINARY_API_KEY'),
+                'api_secret' => env('CLOUDINARY_API_SECRET'),
+            ],
         ],
 
     ],
